a63df3dca275d02a21c08d14bb74712c6023c7d0
Dmitry <dim0n2023@yandex.ru>
2021-04-10 16:14:43 +0300

    add random perspective to augmentations

--- preprocessing.py
+++ preprocessing.py
@@ -3,11 +3,49 @@ import numpy as np
 from PIL import Image, ImageEnhance
 
 
-def random_augment_image(path, rfactor=0.17):
+def random_augment_image(path, rfactor=0.14):
 
     '''
     Returns PIL image.
+
+    Params:
+        path: string. Path to image. Image default converts to RGB formats automaticaly.
+        rfactor: float. 
+
     '''
+
+    def _adjust_random_perspective(image):
+    
+        def _find_coeffs(pa, pb):
+            matrix = []
+            for p1, p2 in zip(pa, pb):
+                matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1]])
+                matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1]*p1[0], -p2[1]*p1[1]])
+
+            A = np.matrix(matrix, dtype=np.float)
+            B = np.array(pb).reshape(8)
+            res = np.linalg.solve(A, B)
+            return np.reshape(res, 8)
+
+        w, h = image.size
+        current_coordinates = np.array([
+            [0, 0],
+            [0, w],
+            [h, w],
+            [h, 0],
+        ])
+        deltas = np.random.randint(size=8, low=0, high=int(0.3*np.max([w, h])))
+        target_coordinates = np.array([
+            [0+deltas[0], 0+deltas[1]],
+            [0+deltas[2], w-deltas[3]],
+            [h-deltas[4], w-deltas[5]],
+            [h-deltas[6], 0+deltas[7]],
+        ])
+        
+        coeffs = _find_coeffs(current_coordinates, target_coordinates)
+        result_image = image.transform(image.size, Image.PERSPECTIVE, coeffs, Image.BICUBIC)
+        return result_image
+    
 
     def _adjust_random_sharpness(image, low=-3.0, high=4.0):
         factor = np.random.uniform(low, high)
@@ -52,5 +90,7 @@ def random_augment_image(path, rfactor=0.17):
         image = _adjust_random_sharpness(image)
     if np.random.rand() > rfactor:
         image = _adjust_random_rotation(image)
+    if np.random.rand() > rfactor:
+        image = _adjust_random_perspective(image)
     
     return image
